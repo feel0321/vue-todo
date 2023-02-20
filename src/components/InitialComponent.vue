4b13723d5a1b3a368988187cf717d1322d72f674
feel0321 <dishate@naver.com>
2023-02-20 23:28:44 +0900

    chore: initial setting

---
+++ src/components/InitialComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>InitialComponent.vue</div>
+</template>
+
+<script>
+export default {
+  name: "InitialComponent",
+};
+</script>
---
+++ src/views/InitialView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>InitialView.vue</div>
+    <InitialComponent />
+  </div>
+</template>
+
+<script>
+import InitialComponent from "../components/InitialComponent.vue";
+
+export default {
+  name: "InitialView",
+  components: {
+    InitialComponent,
+  },
+};
+</script>
